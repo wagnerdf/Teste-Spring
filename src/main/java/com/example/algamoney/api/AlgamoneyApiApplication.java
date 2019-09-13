4a56f17406cbf87c5496089d1d91eefba859b48b
Wagner Andrade <wagnerandrade_df@hotmail.com>
2019-09-13 12:46:34 -0300

    Conectando ao MySQL

--- src/main/java/com/example/algamoney/api/AlgamoneyApiApplication.java
+++ src/main/java/com/example/algamoney/api/AlgamoneyApiApplication.java
@@ -1,5 +1,7 @@
 package com.example.algamoney.api;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,7 @@ public class AlgamoneyApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AlgamoneyApiApplication.class, args);
+		Locale.setDefault(Locale.US);
 	}
 
 }
